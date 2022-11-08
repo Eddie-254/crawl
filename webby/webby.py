5ab7935745fe66c38539fae7269cbcdc243edcac
Eddie-254 <nderituedwin90@gmail.com>
2022-11-08 13:26:25 +0300

    newegg scraper

--- webby/webby.py
+++ webby/webby.py
@@ -2,6 +2,7 @@ import re # regex
 import urllib.request
 from urllib.request import urlopen as uReq
 from bs4 import BeautifulSoup as soup
+import csv
 
 my_url = 'https://www.newegg.com/Video-Cards-Video-Devices/Category/ID-38?Tpk=graphics%20card'
 
@@ -16,26 +17,32 @@ page_soup = soup(kurasa, "html.parser")
 containers = page_soup.findAll("div",{"class":"item-container"})
 
 filename = "Graphics.csv"
-f = open(filename, "w")
-headers = "brand, product_name, shipping \n"
 
-f.write(headers)
+headers = ['Brand', 'Product_name', 'Shipping']
+f = open(filename, 'w')
+# creating a csv dict writer object
+writer = csv.DictWriter(f, fieldnames = headers)
+ # writing headers (field names)
+writer.writeheader()
+
 
 for container in containers:
+
     brand_container = container.findAll("div", {"class":"item-info"})
     brand = brand_container[0].div.a.img["title"]
+    
 
     title_container = container.findAll("a",{"class":"item-title"})
     product_name = title_container[0].text
+    
 
     shipping_container = container.findAll("li",{"class":"price-ship"})
     shipping = shipping_container[0].text.strip()
 
-    print("brand: " + brand) 
-    print("product_name: " + product_name)
-    print("shipping: " + shipping)
-
-    f.write(brand + "," + product_name.replace(",", "|" + "," + shipping + "\n"))
+    prod = [{'Brand': brand, 'Product_name':product_name, 'Shipping':shipping}]
+    
+    # writing data row 
+    writer.writerows(prod)
 f.close()
- 
-
+    
+   
